0ba9dad108edf64c6515f8c9e6757b764c9fa4e7
maiconfa <maiconjfa@hotmail.com>
2021-10-15 21:41:15 -0300

    aula 15-10

---
+++ aula15_10/01.js
@@ -0,0 +1,11 @@
+//primeiro programa
+//console.log('Olá mundo');
+
+var nome = 'Maicon';
+var sobrenome = 'Alves';
+var nomeCompleto;
+/*nomeCompleto = nome + sobrenome;
+console.log(nome + ' ' + sobrenome);*/
+
+var idade = 32;
+console.log('Meu nome é '+nome+' tenho: '+idade);
---
+++ aula15_10/ex.js
@@ -0,0 +1,27 @@
+/*var numum = 5;
+var numdois = 4;
+var numtres = 9;
+var total;
+
+total = numum + (numdois * numtres);
+console.log(total);*/
+
+/*var numum = 5;
+var numdois = 5;
+var numtres = 10;
+var numquatro = 2;
+var total;
+
+total = (numum + numdois) / (numtres * numquatro);
+console.log(total);*/
+
+
+var numum = 20;
+var numdois = 20;
+var numtres = 5;
+var numquatro = 10;
+var total;
+
+total = (numum + numdois) / (numquatro - numtres);
+//total = (numum * numdois) / (numtres * numquatro);
+console.log(total);
